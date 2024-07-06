e4628d6cb5c6948c76408c2f2454817e8ca4c713
VallzPrivate <vallzoffcvall@gmail.com>
2024-07-06 23:02:44 +0700

    Update wings.sh

--- wings.sh
+++ wings.sh
@@ -5,7 +5,7 @@ read -p "Masukkan token untuk menjalankan wings: " token
 
 # Menjalankan perintah dengan menggunakan token yang dimasukkan
 echo "Menjalankan perintah dengan token: $token"
-$token
+eval $token
 
 # Menjalankan perintah systemctl start wings
 sudo systemctl start wings
